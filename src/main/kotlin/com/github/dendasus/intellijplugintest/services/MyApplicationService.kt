8d25bf9ec7b96d60000a6e8cb6d2d2071b18195e
GitHub Action <action@github.com>
2021-09-10 20:11:24 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/dendasus/intellijplugintest/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.dendasus.intellijplugintest.services
+
+import com.github.dendasus.intellijplugintest.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
